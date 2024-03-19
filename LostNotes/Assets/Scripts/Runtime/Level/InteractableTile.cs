8732011eac13af18fa397a216789775477d1e75d
Georg Schäfer <georg.peter.schaefer@gmail.com>
2024-03-19 21:01:27 +0100

    refactor: move interactable tile into own file

---
+++ LostNotes/Assets/Scripts/Runtime/Level/InteractableTile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LostNotes.Level {
+	[CreateAssetMenu(fileName = "InteractableTile", menuName = "InteractableTile", order = 0)]
+	public class InteractableTile : TileBase {
+		[SerializeField] private GameObject _gameObject;
+		
+		public GameObject GetGameObject() {
+			return null;
+		}
+
+		public bool IsWalkable() {
+			return true;
+		}
+	}
+}
